c051f9fffb019ad61c64ac4c57623cb373c17632
Fillipe <fillipeolipe@gmail.com>
2022-03-22 16:55:21 -0300

    Ajustando os botões do modal

--- js/modalController.js
+++ js/modalController.js
@@ -13,17 +13,31 @@ buttonsToOpenModal.forEach((e) => {
 });
 
 buttonsModal[0].addEventListener('click', () => {
-	alert('Horário marcado com sucesso!');
-	modal.classList.remove(openClassModal);
+	let modalImputName = modal.querySelector('.modal__input');
+
+	if (modalImputName.value) {
+		alert(`${modalImputName.value} seu horário foi marcado com sucesso!`);
+		modal.classList.remove(openClassModal);
+		clearTitle();
+	} else {
+		alert('Preencha os campos corretamente!');
+	}
 });
 
+// Botão para cancelar
 buttonsModal[1].addEventListener('click', () => {
 	modal.classList.remove(openClassModal);
-	modalTitle[0].innerText = `Marcar horário para`;
+	clearTitle();
 });
 
 function openModal(title) {
-	modal.classList.add(openClassModal);
-	title = title.toLowerCase();
-	modalTitle[0].innerText += ` ${title}`;
+	if (!modal.classList.contains(openClassModal)) {
+		modal.classList.add(openClassModal);
+		title = title.toLowerCase();
+		modalTitle[0].innerText += ` ${title}`;
+	}
+}
+
+function clearTitle() {
+	modalTitle[0].innerText = `Marcar horário para`;
 }
